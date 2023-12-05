5c05a3989ed7522e0edbdaa9b88172263f384d41
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2023-12-05 18:13:56 +0200

    factory

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'description' => $this->faker->sentence(10),
+            'status' => $this->faker->randomElement(['todo', 'in_progress', 'done', 'tested', 'deployed']),
+            'assigned_to' => $this->faker->numberBetween(1, 10),
+            'created_by' => $this->faker->numberBetween(1, 10),
+            'project_id' => $this->faker->numberBetween(1, 10),
+        ];
+    }
+}
