ca85a83b4a2ca70f2a72b78187bf9b54fedf4996
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2023-12-05 18:11:26 +0200

    migrations

---
+++ database/migrations/2023_12_05_160254_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('description', 1000);
+            $table->enum('status', ['todo', 'in_progress', 'done', 'tested', 'deployed'])->default('todo');
+            $table->foreignId('assigned_to')->nullable()->constrained('users');
+            $table->foreignId('created_by')->nullable()->constrained('users');
+            $table->foreignId('project_id')->nullable()->constrained('projects');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
