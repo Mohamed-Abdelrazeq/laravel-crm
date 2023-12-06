7a4f4574868e266d335cb847e243ca3e0b1cfeb4
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2023-12-06 06:31:24 +0200

    fix migrations

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -7,18 +7,17 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\project>
  */
-class ProjectFactory extends Factory
-{
+class ProjectFactory extends Factory {
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
+    public function definition(): array {
         return [
             'title' => $this->faker->unique()->sentence(1),
             'description' => $this->faker->sentence(10),
+            'user_id' => $this->faker->numberBetween(1, 100)
         ];
     }
 }
--- database/migrations/2023_12_05_160245_create_projects_table.php
+++ database/migrations/2023_12_05_160245_create_projects_table.php
@@ -8,12 +8,12 @@ return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('title', 100)->unique();
             $table->string('description', 1000);
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
@@ -21,8 +21,7 @@ return new class extends Migration {
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('projects');
     }
 };
