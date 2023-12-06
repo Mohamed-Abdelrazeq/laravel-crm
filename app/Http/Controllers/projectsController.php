18b63850b53e031cdd792806791936f6d4063dc8
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2023-12-06 06:33:45 +0200

    store and delete

--- app/Http/Controllers/projectsController.php
+++ app/Http/Controllers/projectsController.php
@@ -5,45 +5,37 @@ namespace App\Http\Controllers;
 use App\Models\project;
 use Illuminate\Http\Request;
 
-class projectsController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
+class projectsController extends Controller {
+    public function index() {
         return project::paginate(5);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request) {
+        $project = Project::create([
+            ...$request->validate([
+                'title' => 'required|string|max:255',
+                'description' => 'required|string|max:1000',
+            ]),
+            'user_id' => 1,
+        ]);
+        return response()->json([
+            'message' => 'Project created successfully',
+            'project' => $project
+        ]);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(project $project)
-    {
+    public function show(project $project) {
         return $project;
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
+    public function update(Request $request, string $id) {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+    public function destroy(project $project) {
+        $project->delete();
+        return response()->json([
+            'message' => 'Project deleted successfully'
+        ]);
     }
 }
